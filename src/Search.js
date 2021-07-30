0adb63fcb8a114e426001023447f2aa85d162641
P811187 <Leesa.Ward@nab.com.au>
2021-07-30 16:21:34 +1000

    Formatting

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,16 @@
 import React from "react";
 
 export default function Search() {
-  return (
-    <form id="search-form">
-      <label htmlFor="search" className="visually-hidden">
-        Search for a city
-      </label>
-      <input
-        id="search"
-        type="search"
-        name="search"
-        placeholder="Search for a city"
-      />
-      <button type="submit">
-        <span className="visually-hidden">Search</span>
-        <span className="material-icons">search</span>
-      </button>
-    </form>
-  );
+	return (
+		<form id="search-form">
+			<label htmlFor="search" className="visually-hidden">
+				Search for a city
+			</label>
+			<input id="search" type="search" name="search" placeholder="Search for a city"/>
+			<button type="submit">
+				<span className="visually-hidden">Search</span>
+				<span className="material-icons">search</span>
+			</button>
+		</form>
+	);
 }
