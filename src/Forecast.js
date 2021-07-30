2213189205787e02b49374195440130f0b78c47b
P811187 <Leesa.Ward@nab.com.au>
2021-07-30 11:31:22 +1000

    Use fragment to include the heading in the forecast component

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,11 +1,16 @@
+import React from 'react';
+
 export default function Forecast() {
-  return (
-    <div id="forecast">
-      <div class="forecast-item"></div>
-      <div class="forecast-item"></div>
-      <div class="forecast-item"></div>
-      <div class="forecast-item"></div>
-      <div class="forecast-item"></div>
-    </div>
-  );
+	return (
+		<React.Fragment>
+			<h2>This week</h2>
+			<div id="forecast">
+				<div class="forecast-item"></div>
+				<div class="forecast-item"></div>
+				<div class="forecast-item"></div>
+				<div class="forecast-item"></div>
+				<div class="forecast-item"></div>
+			</div>
+		</React.Fragment>
+	);
 }
