4e33b9b3e92d572151794eda8bbed3cd4fedb034
P811187 <Leesa.Ward@nab.com.au>
2021-07-30 11:43:27 +1000

    Formatting

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 export default function Temperature(props) {
-  return (
-    <a className="today-temp">
-      <div className="today-image-wrap">
-        <img className="today-image" src="" alt="" />
-      </div>
-      <span className="amount" data-temp-amount="{props.degrees}">
-        {props.degrees}&deg;
-      </span>
-      <span className="units" data-temp-units="{props.units}">
-        {props.units}
-      </span>
-    </a>
-  );
+	
+	return (
+		<a className="today-temp">
+			<div className="today-image-wrap">
+				<img className="today-image" src="" alt=""/>
+			</div>
+			<span className="amount" data-temp-amount="{props.degrees}">{props.degrees}&deg;</span>
+			<span className="units" data-temp-units="{props.units}">{props.units}</span>
+		</a>
+	);
 }
