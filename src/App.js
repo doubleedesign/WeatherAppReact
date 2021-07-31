62c19f791d640e69334146e2179462e885479566
P811187 <Leesa.Ward@nab.com.au>
2021-08-01 01:34:05 +1000

    Formatting

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import Forecast from "./Forecast";
 import React from "react";
 
 export default function App() {
-  return (
-    <main id="weather">
-      <section className="row">
-        <Search />
-      </section>
-      <section className="row">
-        <Title city="New York" />
-      </section>
-      <section className="row">
-        <Today />
-      </section>
-      <section className="row">-
-        <Forecast />
-      </section>
-    </main>
-  );
+
+
+	return (
+		<main id="weather">
+			<section className="row">
+				<Search/>
+			</section>
+			<section className="row">
+				<Title city="New York"/>
+			</section>
+			<section className="row">
+				<Today/>
+			</section>
+			<section className="row">-
+				<Forecast/>
+			</section>
+		</main>
+	);
 }
--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Search() {
+    let [city, setCity] = useState('');
+
+    function handleSearch(event) {
+        event.preventDefault();
+    }
+
+    function updateCity(event) {
+        setCity(event.target.value);
+    }
+
 	return (
-		<form id="search-form">
-			<label htmlFor="search" className="visually-hidden">
-				Search for a city
-			</label>
-			<input id="search" type="search" name="search" placeholder="Search for a city"/>
+		<form id="search-form" onSubmit={handleSearch}>
+			<label htmlFor="search" className="visually-hidden">Search for a city</label>
+			<input id="search" type="search" name="search" placeholder="Search for a city" onChange={updateCity}/>
 			<button type="submit">
 				<span className="visually-hidden">Search</span>
 				<span className="material-icons">search</span>
