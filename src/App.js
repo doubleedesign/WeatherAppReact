803cc7016f94b01a6a5126b50abedd23116a17d0
P811187 <Leesa.Ward@nab.com.au>
2021-08-01 13:13:19 +1000

    Implement styling change on temperature change by passing data up to the parent component; same for outputting returned city name

--- src/App.js
+++ src/App.js
@@ -8,20 +8,48 @@ import Forecast from "./Forecast";
 import React, {useState} from "react";
 
 export default function App() {
-	let [city, setCity] = useState('');
 
 	// When a city is searched for, set the state so child components can pick it up
 	// Ref: https://www.geeksforgeeks.org/how-to-pass-data-from-one-component-to-other-component-in-reactjs/
-	function onCityChange(searchedCity) {
-		setCity(searchedCity);
+	let [searchTerm, setSearchTerm] = useState('');
+	function onSearchChange(searchedCity) {
+		setSearchTerm(searchedCity);
+	}
+
+	/**
+	 * The today component sends up some selected data from the weather query so it can be used by other components
+	 * Here we update the state variables used by those components when new data is received
+	 */
+	let [city, setCity] = useState('');
+	let [tempRange, setTempRange] = useState('');
+	function onWeatherChange(data) {
+
+		// Display the returned city name rather than exactly what was searched
+		setCity(data.city);
+
+		// Set the data-temp-range attribute for styling purposes
+		if(data.temperature <= 15) {
+			setTempRange('cold');
+		}
+		else if(data.temperature > 15 && data.temperature < 25 ) {
+			setTempRange('fair');
+		}
+		else if(data.temperature >= 25 < 35) {
+			setTempRange('warm');
+		}
+		else {
+			setTempRange('hot');
+		}
 	}
 
 	return (
-		<main id="weather">
-			<Search parentCallback = {onCityChange}/>
-			<Title city={city}/>
-			<Today city={city}/>
-			<Forecast city={city}/>
-		</main>
+		<div className="wrapper" data-temp-range={tempRange}>
+			<main id="weather">
+				<Search parentCallback={onSearchChange} />
+				<Title city={city} parentCallback={onWeatherChange} />
+				<Today city={searchTerm} parentCallback={onWeatherChange} />
+				<Forecast city={searchTerm} />
+			</main>
+		</div>
 	);
 }
--- src/Today.js
+++ src/Today.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect, useState} from "react";
+import React, {Fragment, useEffect, useMemo, useState} from "react";
 import Time from "./Time";
 import Temperature from "./Temperature";
 import Details from "./Details";
@@ -8,22 +8,49 @@ export default function Today(props) {
 	const apiKey = 'f4f65838c4d2f2b467cb557338c7cc7c';
 	let [weather, setWeather] = useState(null);
 
-	// Set and perform API query
-	// This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.city)
-	// otherwise it runs in an infinite loop and we have a bad time
+	/**
+	 * Set and perform API query
+	 * This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.city)
+	 * otherwise it runs in an infinite loop and we have a bad time
+	 */
 	useEffect(() => {
-		if (props.city) {
-			let query = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-			axios.get(query)
-				// Update component state when an API response is received
-				// Catch and log error if there is one
-				.then(response => {
-					setWeather(response.data)
-				}).catch(error => {
-					console.log(error);
-				})
+		let query = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+		axios.get(query)
+			// Update component state when an API response is received
+			// Catch and log error if there is one
+			.then(response => {
+				setWeather(response.data);
+				/**
+				 * NOTES:
+				 * sending weather.main.temp up to the parent via
+				 * props.parentCallback(Math.round(weather.main.temp)) doesn't work here,
+				 * because the weather variable still contains the previous data even though it was just set...
+				 * Sending the response data directly works, e.g. response.data.weather.main.temp,
+				 * but I thought it was better to try to work out how to get it from the weather variable at the right time
+				**/
+			}).catch(error => {
+				console.log(error);
+				alert('Sorry, couldn\'t find that city. Please try again');
+			})
+	}, [props.city]);
+
+	/**
+	 * Second hook to send the temperature and returned city name up to the parent component after the weather state variable updates.
+	 * Can't do this in the same useEffect as the query because the weather variable still contains the previous data then.
+	 *
+	 * Why send the returned city name given the user just typed the city in, you ask?
+	 * The user could type in lowercase, could need to type a qualifier (e.g. need to use 'Melbourne AU' or else you get Melbourne Florida)
+	 * Using the returned one ensures the output is written as expected in the title component
+	 */
+	useEffect(() => {
+		if(weather) {
+			let data = {
+				city: weather.name,
+				temperature: Math.round(weather.main.temp)
+			}
+			props.parentCallback(data);
 		}
-	}, [props.city])
+	}, [weather])
 
 	// Put output in a variable so it can be shown conditionally
 	// (in the component's return statement)
