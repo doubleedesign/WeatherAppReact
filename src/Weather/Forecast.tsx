dcfd1a14e76adc4f69f6b35155cc918cf79a06b4
Leesa Ward <Leesa.Ward@nab.com.au>
2021-08-10 23:52:29 +1000

    Formatting

--- src/Weather/Forecast.tsx
+++ src/Weather/Forecast.tsx
@@ -4,216 +4,218 @@ import Temperature from "./Temperature";
 import "./_Forecast.scss";
 
 export interface ForecastProps {
-	coords: { lat: number; lon: number; };
-	units: React.SetStateAction<string>;
+    coords: { lat: number; lon: number; };
+    units: React.SetStateAction<string>;
 }
 
-export const Forecast: React.FC<ForecastProps> = function(
-		props: {
-			coords: { lat: number; lon: number; };
-			units: React.SetStateAction<string>;
-		}
-	){
-	const apiKey = 'f4f65838c4d2f2b467cb557338c7cc7c';
-	const [forecast, setForecast] = useState({} as any); // https://basarat.gitbook.io/typescript/main-1/lazyobjectliteralinitialization
-	const [units, setUnits] = useState('C'); // See Temperature.tsx for notes about using state
+export const Forecast: React.FC<ForecastProps> = function (
+    props: {
+        coords: { lat: number; lon: number; };
+        units: React.SetStateAction<string>;
+    }
+) {
+    const apiKey = 'f4f65838c4d2f2b467cb557338c7cc7c';
+    const [forecast, setForecast] = useState({} as any); // https://basarat.gitbook.io/typescript/main-1/lazyobjectliteralinitialization
+    const [units, setUnits] = useState('C'); // See Temperature.tsx for notes about using state
 
-	/**
-	 * Create and use useDidMountEffect hook with useRef
-	 * for useEffect stuff that we do not want to run on first render
-	 * Ref: https://thewebdev.info/2021/03/13/how-to-make-the-react-useeffect-hook-not-run-on-initial-render/
-	 */
-	const useDidMountEffect = () => {
-		const didMount = useRef(false);
+    /**
+     * Create and use useDidMountEffect hook with useRef
+     * for useEffect stuff that we do not want to run on first render
+     * Ref: https://thewebdev.info/2021/03/13/how-to-make-the-react-useeffect-hook-not-run-on-initial-render/
+     */
+    const useDidMountEffect = () => {
+        const didMount = useRef(false);
 
-		/**
-		 * Update the forecast when the city changes
-		 * This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.coords)
-		 */
-		useEffect(() => {
-			if (didMount.current) {
-				getForecastForCity(props.coords);
-				setUnits(props.units)
-			} else {
-				didMount.current = true;
-			}
-		}, [props.coords, props.units]);
-	}
-	useDidMountEffect();
+        /**
+         * Update the forecast when the city changes
+         * This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.coords)
+         */
+        useEffect(() => {
+            if (didMount.current) {
+                getForecastForCity(props.coords);
+                setUnits(props.units)
+            } else {
+                didMount.current = true;
+            }
+        }, [props.coords, props.units]);
+    }
+    useDidMountEffect();
 
-	/**
-	 * Set and perform API query to get the forecast for a given city
-	 * and save it in the state variable
-	 * Note: Forecast query requires coordinates, not city name
-	 * @param coords
-	 */
-	function getForecastForCity(coords: { lat: number; lon: number; }) {
-		const query = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coords.lat}&lon=${props.coords.lon}&appid=${apiKey}&units=metric&exclude=current,hourly,minutely,alerts`;
+    /**
+     * Set and perform API query to get the forecast for a given city
+     * and save it in the state variable
+     * Note: Forecast query requires coordinates, not city name
+     * @param coords
+     */
+    function getForecastForCity(coords: { lat: number; lon: number; }) {
+        const query = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coords.lat}&lon=${props.coords.lon}&appid=${apiKey}&units=metric&exclude=current,hourly,minutely,alerts`;
 
-		axios.get(query)
-			// Update component state when an API response is received
-			// Catch and log error if there is one
-			.then(response => {
+        axios.get(query)
+            // Update component state when an API response is received
+            // Catch and log error if there is one
+            .then(response => {
 
-				// Slice the returned data down to only the first five days
-				let fullData = response.data.daily;
-				fullData = fullData.slice(1, 6);
+                // Slice the returned data down to only the first five days
+                let fullData = response.data.daily;
+                fullData = fullData.slice(1, 6);
 
-				// Loop through the returned data and put just what we want into an object
-				const summaryData: { [key: string]: any } = {};
-				{fullData.map((value: { dt: any; temp: { min: number; max: number; }; weather: { icon: any; }[]; }, index: any) => {
-					// Get current timezone offset
-					const today = new Date();
-					const offset = today.getTimezoneOffset();
+                // Loop through the returned data and put just what we want into an object
+                const summaryData: { [key: string]: any } = {};
+                {
+                    fullData.map((value: { dt: any; temp: { min: number; max: number; }; weather: { icon: any; }[]; }, index: any) => {
+                        // Get current timezone offset
+                        const today = new Date();
+                        const offset = today.getTimezoneOffset();
 
-					// Get the forecast day's unix timestamp from the returned data
-					const timestamp = value.dt;
+                        // Get the forecast day's unix timestamp from the returned data
+                        const timestamp = value.dt;
 
-					// Get the day of that timestamp taking the timezone offset into account
-					const date = new Date((timestamp * 1000) + (offset * 60 * 1000));
-					const dayNo = date.getDay();
-					const day = getDayAbbrev(dayNo);
+                        // Get the day of that timestamp taking the timezone offset into account
+                        const date = new Date((timestamp * 1000) + (offset * 60 * 1000));
+                        const dayNo = date.getDay();
+                        const day = getDayAbbrev(dayNo);
 
-					// Add this forecast day's data to the summary
-					const minC = Math.round(value.temp.min);
-					const maxC = Math.round(value.temp.max);
-					summaryData[day] = {
-						C: {
-							min: minC,
-							max: maxC
-						},
-						F: {
-							min: convertTemperature(minC, 'C', 'F'),
-							max: convertTemperature(minC, 'C', 'F'),
-						},
-						imageUrl: `http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`
-					}
-				})}
+                        // Add this forecast day's data to the summary
+                        const minC = Math.round(value.temp.min);
+                        const maxC = Math.round(value.temp.max);
+                        summaryData[day] = {
+                            C: {
+                                min: minC,
+                                max: maxC
+                            },
+                            F: {
+                                min: convertTemperature(minC, 'C', 'F'),
+                                max: convertTemperature(minC, 'C', 'F'),
+                            },
+                            imageUrl: `http://openweathermap.org/img/wn/${value.weather[0].icon}@2x.png`
+                        }
+                    })
+                }
 
-				// Update the forecast state variable with our customised data object
-				setForecast(summaryData);
+                // Update the forecast state variable with our customised data object
+                setForecast(summaryData);
 
-			}).catch(error => {
-				console.log(error);
-			})
-	}
+            }).catch(error => {
+            console.log(error);
+        })
+    }
 
-	/**
-	 * Utility function to convert the day number to its 3-letter abbreviation
-	 * @param number
-	 * @returns {string}
-	 */
-	function getDayAbbrev(number: number) {
-		let day = '';
-		switch(number) {
-			case 0:
-				day = 'Sun';
-				break;
-			case 1:
-				day = 'Mon';
-				break;
-			case 2:
-				day = 'Tue';
-				break;
-			case 3:
-				day = 'Wed';
-				break;
-			case 4:
-				day = 'Thu';
-				break;
-			case 5:
-				day = 'Fri';
-				break;
-			case 6:
-				day = 'Sat';
-				break;
-			default:
-				break;
-		}
+    /**
+     * Utility function to convert the day number to its 3-letter abbreviation
+     * @param number
+     * @returns {string}
+     */
+    function getDayAbbrev(number: number) {
+        let day = '';
+        switch (number) {
+            case 0:
+                day = 'Sun';
+                break;
+            case 1:
+                day = 'Mon';
+                break;
+            case 2:
+                day = 'Tue';
+                break;
+            case 3:
+                day = 'Wed';
+                break;
+            case 4:
+                day = 'Thu';
+                break;
+            case 5:
+                day = 'Fri';
+                break;
+            case 6:
+                day = 'Sat';
+                break;
+            default:
+                break;
+        }
 
-		return day;
-	}
+        return day;
+    }
 
-	/**
-	 * Utility function to convert temperatures between C and F
-	 * @param temp
-	 * @param unitsFrom
-	 * @param unitsTo
-	 * @returns {number}
-	 */
-	function convertTemperature(temp: number, unitsFrom: string, unitsTo: string) {
-		let newTemp = temp;
+    /**
+     * Utility function to convert temperatures between C and F
+     * @param temp
+     * @param unitsFrom
+     * @param unitsTo
+     * @returns {number}
+     */
+    function convertTemperature(temp: number, unitsFrom: string, unitsTo: string) {
+        let newTemp = temp;
 
-		if (unitsFrom === 'C' && unitsTo === 'F') {
-			newTemp = Math.round((temp * 1.8) + 32);
-		} else if (unitsFrom === 'F' && unitsTo === 'C') {
-			newTemp = Math.round((temp - 32) * 0.5556);
-		}
+        if (unitsFrom === 'C' && unitsTo === 'F') {
+            newTemp = Math.round((temp * 1.8) + 32);
+        } else if (unitsFrom === 'F' && unitsTo === 'C') {
+            newTemp = Math.round((temp - 32) * 0.5556);
+        }
 
-		return newTemp;
-	}
+        return newTemp;
+    }
 
-	/**
-	 * What to do if the temperature component sends data up using the onUnitUpdate prop
-	 * This does more in the Today component because that one uses clickable Temperatures whereas this one doesn't
-	 * TODO: Refactor so there's less duplication and so this component can handle clickable Temperatures properly too
-	 * @param unitsTo
-	 * @param newTemp
-	 */
-	function switchUnits(unitsTo: string, newTemp: React.SetStateAction<number>) {
-		// Update the state in this component
-		setUnits(unitsTo);
-	}
+    /**
+     * What to do if the temperature component sends data up using the onUnitUpdate prop
+     * This does more in the Today component because that one uses clickable Temperatures whereas this one doesn't
+     * TODO: Refactor so there's less duplication and so this component can handle clickable Temperatures properly too
+     * @param unitsTo
+     * @param newTemp
+     */
+    function switchUnits(unitsTo: string, newTemp: React.SetStateAction<number>) {
+        // Update the state in this component
+        setUnits(unitsTo);
+    }
 
-	/**
-	 * Put output in a variable so it can be shown conditionally
-	 * (in the component's return statement)
-	 * Ref: https://stackoverflow.com/a/24534492
-	 * Can't use Fragment here for some reason...?
-	 * @returns {*}
-	 * @constructor
-	 */
-	const Output = () => {
-		return <section className="forecast row">
-			{ /** Loop through the forecast items and add each one to the page */}
-			{Object.keys(forecast).map(day => (
-				<div className="forecast__item" key={day}>
-					<span className="forecast__item__day">{day}</span>
-					<img className="forecast__item__image" src={forecast[day].imageUrl} alt=""/>
-					<span className="forecast__item__min">
+    /**
+     * Put output in a variable so it can be shown conditionally
+     * (in the component's return statement)
+     * Ref: https://stackoverflow.com/a/24534492
+     * Can't use Fragment here for some reason...?
+     * @returns {*}
+     * @constructor
+     */
+    const Output = () => {
+        return <section className="forecast row">
+            { /** Loop through the forecast items and add each one to the page */}
+            {Object.keys(forecast).map(day => (
+                <div className="forecast__item" key={day}>
+                    <span className="forecast__item__day">{day}</span>
+                    <img className="forecast__item__image" src={forecast[day].imageUrl} alt=""/>
+                    <span className="forecast__item__min">
 						Low
 						<Temperature
-							size="small"
-							degrees={forecast[day][units].min}
-							units={units}
-							showUnits={false}
-							clickable={false}
-						 	onUnitUpdate={switchUnits}/>
+                            size="small"
+                            degrees={forecast[day][units].min}
+                            units={units}
+                            showUnits={false}
+                            clickable={false}
+                            onUnitUpdate={switchUnits}/>
 					</span>
-					<span className="forecast__item__max">
+                    <span className="forecast__item__max">
 						High
 						<Temperature
-							size="small"
-							degrees={forecast[day][units].max}
-							units={units}
-							showUnits={false}
-							clickable={false}
-						 	onUnitUpdate={switchUnits}/>
+                            size="small"
+                            degrees={forecast[day][units].max}
+                            units={units}
+                            showUnits={false}
+                            clickable={false}
+                            onUnitUpdate={switchUnits}/>
 					</span>
-				</div>
-			))}
-		</section>;
-	}
+                </div>
+            ))}
+        </section>;
+    }
 
-	/**
-	 * Output - only shown when forecast exists
-	 * Ref: https://stackoverflow.com/a/24534492
-	 */
-	return (
-		<Fragment>
-			{forecast ? <Output/> : null}
-		</Fragment>
-	);
+    /**
+     * Output - only shown when forecast exists
+     * Ref: https://stackoverflow.com/a/24534492
+     */
+    return (
+        <Fragment>
+            {forecast ? <Output/> : null}
+        </Fragment>
+    );
 }
 
 export default Forecast;
--- src/Weather/Today.tsx
+++ src/Weather/Today.tsx
@@ -5,194 +5,196 @@ import axios from "axios";
 import "./_Today.scss";
 
 export interface TodayProps {
-	units: string;
-	city: string,
-	onWeatherUpdate(data: { country: any; city: any; temperature: number; units: string; coords: {lat: number, lon: number} }): void;
-	onUnitUpdate(unitsTo: string): void;
+    units: string;
+    city: string,
+
+    onWeatherUpdate(data: { country: any; city: any; temperature: number; units: string; coords: { lat: number, lon: number } }): void;
+
+    onUnitUpdate(unitsTo: string): void;
 }
 
 type WeatherResponse = // ref: https://github.com/microsoft/TypeScript/issues/20597
-	{
-		wind: any;
-		weather: any;
-		name: string,
-		sys: any,
-		coord: {
-			lat: number,
-			lon: number
-		},
-		main: {
-			humidity: any;
-			temp: number
-		}
-	};
+    {
+        wind: any;
+        weather: any;
+        name: string,
+        sys: any,
+        coord: {
+            lat: number,
+            lon: number
+        },
+        main: {
+            humidity: any;
+            temp: number
+        }
+    };
 
 
-export const Today: React.FC<TodayProps> = function(
-	props: {
-		units: string;
-		city: string,
-		onWeatherUpdate(data: { country: string; city: string; temperature: number; units: string; coords: { lat: number; lon: number } }): void;
-		onUnitUpdate(unitsTo: string): void;
-	}) {
-	const apiKey = 'f4f65838c4d2f2b467cb557338c7cc7c';
-	const emptyResponse: WeatherResponse = {
-		wind: 0,
-		weather: {},
-		name: '',
-		sys: {},
-		coord: {
-			lat: 0,
-			lon: 0
-		},
-		main: {
-			humidity: 0,
-			temp: 0
-		}
-	}
-	const [weather, setWeather] = useState(emptyResponse);
+export const Today: React.FC<TodayProps> = function (
+    props: {
+        units: string;
+        city: string,
+        onWeatherUpdate(data: { country: string; city: string; temperature: number; units: string; coords: { lat: number; lon: number } }): void;
+        onUnitUpdate(unitsTo: string): void;
+    }) {
+    const apiKey = 'f4f65838c4d2f2b467cb557338c7cc7c';
+    const emptyResponse: WeatherResponse = {
+        wind: 0,
+        weather: {},
+        name: '',
+        sys: {},
+        coord: {
+            lat: 0,
+            lon: 0
+        },
+        main: {
+            humidity: 0,
+            temp: 0
+        }
+    }
+    const [weather, setWeather] = useState(emptyResponse);
 
-	// Temperature needs to be stored separately because it can be changed
-	// (Temperature component has a C/F conversion option which breaks without this
-	// unless we weren't going to send the unit change between the various components)
-	const [temperature, setTemperature] = useState(0);
-	const [units, setUnits] = useState(props.units);
+    // Temperature needs to be stored separately because it can be changed
+    // (Temperature component has a C/F conversion option which breaks without this
+    // unless we weren't going to send the unit change between the various components)
+    const [temperature, setTemperature] = useState(0);
+    const [units, setUnits] = useState(props.units);
 
-	/**
-	 * Create and use useDidMountEffect hook with useRef
-	 * for useEffect stuff that we do not want to run on first render
-	 * Ref: https://thewebdev.info/2021/03/13/how-to-make-the-react-useeffect-hook-not-run-on-initial-render/
-	 */
-	const useDidMountEffect = () => {
-		const didMount = useRef(false);
+    /**
+     * Create and use useDidMountEffect hook with useRef
+     * for useEffect stuff that we do not want to run on first render
+     * Ref: https://thewebdev.info/2021/03/13/how-to-make-the-react-useeffect-hook-not-run-on-initial-render/
+     */
+    const useDidMountEffect = () => {
+        const didMount = useRef(false);
 
-		/**
-		 * Update the weather when the city changes
-		 * This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.city)
-		 * otherwise the API query runs in an infinite loop and we have a bad time.
-		 */
-		useEffect(() => {
-			if (didMount.current) {
-				getWeatherForCity(props.city);
-			} else {
-				didMount.current = true;
-			}
-		}, [props.city]);
-	}
-	useDidMountEffect();
+        /**
+         * Update the weather when the city changes
+         * This is done inside useEffect so it only re-runs if certain values have changed (in this case, props.city)
+         * otherwise the API query runs in an infinite loop and we have a bad time.
+         */
+        useEffect(() => {
+            if (didMount.current) {
+                getWeatherForCity(props.city);
+            } else {
+                didMount.current = true;
+            }
+        }, [props.city]);
+    }
+    useDidMountEffect();
 
-	/**
-	 * Second useEffect hook to send the temperature, units, and returned city name up to the parent component after the weather state variable updates.
-	 * Can't do this in the same useEffect as the query because the weather variable still contains the previous data then.
-	 *
-	 * Why send the returned city name given the user just typed the city in, you ask?
-	 * The user could type in lowercase, could need to type a qualifier (e.g. need to use 'Melbourne AU' or else you get Melbourne Florida)
-	 * Using the returned one ensures the output is written as expected in the title component
-	 */
-	useEffect(() => {
-		if(weather) {
-			const data = {
-				city: weather.name,
-				country: weather.sys.country,
-				coords: weather.coord,
-				temperature: Math.round(weather.main.temp),
-				units: units
-			}
-			props.onWeatherUpdate(data);
-		}
-	}, [weather])
+    /**
+     * Second useEffect hook to send the temperature, units, and returned city name up to the parent component after the weather state variable updates.
+     * Can't do this in the same useEffect as the query because the weather variable still contains the previous data then.
+     *
+     * Why send the returned city name given the user just typed the city in, you ask?
+     * The user could type in lowercase, could need to type a qualifier (e.g. need to use 'Melbourne AU' or else you get Melbourne Florida)
+     * Using the returned one ensures the output is written as expected in the title component
+     */
+    useEffect(() => {
+        if (weather) {
+            const data = {
+                city: weather.name,
+                country: weather.sys.country,
+                coords: weather.coord,
+                temperature: Math.round(weather.main.temp),
+                units: units
+            }
+            props.onWeatherUpdate(data);
+        }
+    }, [weather])
 
-	/**
-	 * When the units prop is updated, set the state
-	 * This ensures that when the units are reset to C in App, that flows through to today's Temperature component
-	 */
-	useEffect(() => {
-		setUnits(props.units);
-	}, [props.units])
+    /**
+     * When the units prop is updated, set the state
+     * This ensures that when the units are reset to C in App, that flows through to today's Temperature component
+     */
+    useEffect(() => {
+        setUnits(props.units);
+    }, [props.units])
 
-	/**
-	 * What to do if the temperature component sends data up using the onUnitUpdate prop
-	 * @param unitsTo
-	 * @param newTemp
-	 */
-	function switchUnits(unitsTo: string, newTemp: React.SetStateAction<number>) {
-		// Update the state in this component
-		setUnits(unitsTo);
-		setTemperature(newTemp);
+    /**
+     * What to do if the temperature component sends data up using the onUnitUpdate prop
+     * @param unitsTo
+     * @param newTemp
+     */
+    function switchUnits(unitsTo: string, newTemp: React.SetStateAction<number>) {
+        // Update the state in this component
+        setUnits(unitsTo);
+        setTemperature(newTemp);
 
-		// Send the units it returns up to the parent component
-		props.onUnitUpdate(unitsTo);
-	}
+        // Send the units it returns up to the parent component
+        props.onUnitUpdate(unitsTo);
+    }
 
-	/**
-	 * Set and perform API query to get the weather for a given city
-	 * and save it in the state variable
-	 * @param city
-	 */
-	function getWeatherForCity(city: string) {
-		const query = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-		axios.get(query)
-			// Update component state when an API response is received
-			// Catch and log error if there is one
-			.then(response => {
-				setWeather(response.data);
-				const roundedTemp = Math.round(response.data.main.temp);
-				setTemperature(roundedTemp);
-				/**
-				 * NOTES:
-				 * sending weather.main.temp up to the parent via
-				 * props.onWeatherUpdate(Math.round(weather.main.temp)) doesn't work here,
-				 * because the weather variable still contains the previous data even though it was just set...
-				 * Sending the response data directly works, e.g. response.data.weather.main.temp,
-				 * but I thought it was better to try to work out how to get it from the weather variable at the right time
-				 **/
-			}).catch(error => {
-				console.log(error);
-				alert('Sorry, couldn\'t find that city. Please try again');
-			})
-	}
+    /**
+     * Set and perform API query to get the weather for a given city
+     * and save it in the state variable
+     * @param city
+     */
+    function getWeatherForCity(city: string) {
+        const query = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+        axios.get(query)
+            // Update component state when an API response is received
+            // Catch and log error if there is one
+            .then(response => {
+                setWeather(response.data);
+                const roundedTemp = Math.round(response.data.main.temp);
+                setTemperature(roundedTemp);
+                /**
+                 * NOTES:
+                 * sending weather.main.temp up to the parent via
+                 * props.onWeatherUpdate(Math.round(weather.main.temp)) doesn't work here,
+                 * because the weather variable still contains the previous data even though it was just set...
+                 * Sending the response data directly works, e.g. response.data.weather.main.temp,
+                 * but I thought it was better to try to work out how to get it from the weather variable at the right time
+                 **/
+            }).catch(error => {
+            console.log(error);
+            alert('Sorry, couldn\'t find that city. Please try again');
+        })
+    }
 
-	/**
-	 * Put output in a variable so it can be shown conditionally
-	 * (in the component's return statement)
-	 * Ref: https://stackoverflow.com/a/24534492
-	 * @returns {*}
-	 * @constructor
-	 */
-	const Output = () => (
-		<section className="today row">
-			<div className="today__text">
-				<div className="today__text__temperature">
-					<div className="today__text__temperature__image-wrap">
-						<img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-							 alt={weather.weather[0].description}/>
-					</div>
-					<Temperature
-						size="large"
-						degrees={temperature}
-						units={units}
-						showUnits={true}
-						clickable={true}
-						onUnitUpdate={switchUnits}
-					/>
-				</div>
-				<Details description={weather.weather[0].description}
-						 humidity={weather.main.humidity}
-						 wind={weather.wind.speed}
-				/>
-			</div>
-		</section>
-	)
+    /**
+     * Put output in a variable so it can be shown conditionally
+     * (in the component's return statement)
+     * Ref: https://stackoverflow.com/a/24534492
+     * @returns {*}
+     * @constructor
+     */
+    const Output = () => (
+        <section className="today row">
+            <div className="today__text">
+                <div className="today__text__temperature">
+                    <div className="today__text__temperature__image-wrap">
+                        <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                             alt={weather.weather[0].description}/>
+                    </div>
+                    <Temperature
+                        size="large"
+                        degrees={temperature}
+                        units={units}
+                        showUnits={true}
+                        clickable={true}
+                        onUnitUpdate={switchUnits}
+                    />
+                </div>
+                <Details description={weather.weather[0].description}
+                         humidity={weather.main.humidity}
+                         wind={weather.wind.speed}
+                />
+            </div>
+        </section>
+    )
 
-	/**
-	 * Output - only shown when weather exists
-	 * Ref: https://stackoverflow.com/a/24534492
-	 */
-	return (
-		<Fragment>
-			{weather ? <Output/> : null}
-		</Fragment>
-	)
+    /**
+     * Output - only shown when weather exists
+     * Ref: https://stackoverflow.com/a/24534492
+     */
+    return (
+        <Fragment>
+            {weather ? <Output/> : null}
+        </Fragment>
+    )
 }
 
 export default Today;
